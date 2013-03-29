c904c95714e60b8e29d60a24920a30d2778b7204
manu <manu@silkinteractive.hk>
2013-03-29 19:45:02 +0100

    Problem 13

--- crockfrod-problems.js
+++ crockfrod-problems.js
@@ -1,14 +1,14 @@
 // Douglas crocford problems from frontendmaster class
 // https://frontendmasters.com/courses/javascript-the-good-parts/
 
-//write a function that takes an argument returns that argument
+// write a function that takes an argument returns that argument
 // identity(3) // 3
 
 function identity( arg ) {
 	return arg;
 }
 
-//Write two birnary functionm add and mul that take two numbers and return their sum and product
+// Write two birnary function add and mul that take two numbers and return their sum and product
 
 function add( a, b ) {
 	return a + b;
@@ -18,7 +18,7 @@ function mul( a, b) {
 	return a * b;
 }
 
-//write a function that take an argument and return a function that returns that arguement
+// write a function that take an argument and return a function that returns that argument
 // var idf = identifyf(3)
 // idf() // 3
 
@@ -28,7 +28,7 @@ function identityf( arg ) {
 	};
 }
 
-//wite a function that adds form two invocations
+// write a function that adds form two invocations
 // addf(3)(4) // 7
 
 function addf( a ) {
@@ -49,7 +49,7 @@ function applyf( func ) {
 	};
 }
 
-//write a function that takes a fucntion and argument and return a fuvcntion that can supply a second argument
+// write a function that takes a function and argument and return a function that can supply a second argument
 // add3 = curry(add, 3)
 // add3(4) //7
 
@@ -57,9 +57,9 @@ function curry( func, arg) {
 	return applyf( func )( arg );
 }
 
-//without adding any new functions, show three ways to create the inc function
-//inc(5) //6
-//inc(inc(5)) // 7
+// without adding any new functions, show three ways to create the inc function
+// inc(5) //6
+// inc(inc(5)) // 7
 
 function inc( x ) { 
 	return addf(1);
@@ -68,7 +68,7 @@ function inc( x ) {
 }
 
 
-// Write methodize, a function that converts a binary funciton to a method
+// Write methodize, a function that converts a binary function to a method
 // Number.prototyp.add = methodize(add);
 // (3).add(4) //7
 
@@ -78,8 +78,8 @@ function methodize(func) {
 		};
 }
 
-//write demethodize a function that converts a method to a binary function
-//demethodize(Number.prototype.add)(5,6) //11
+// write demethodize a function that converts a method to a binary function
+// demethodize(Number.prototype.add)(5,6) //11
 
 function demethodize( meth ) {
 	var that = this;
@@ -93,7 +93,7 @@ function demethodize( meth ) {
 // var double = twice(add)
 // double(11) //22
 // var square = twice(mul)
-//square(11) //121
+// square(11) //121
 
 function twice( func ) {
 	return function( x ) {
@@ -104,7 +104,7 @@ function twice( func ) {
 var square = twice(mul);
 var doublef = twice(add);
 
-//write a function composeu that take two unary function and returns a unaray function that call them both so :
+// write a function composeu that take two unary function and returns a unary function that call them both so :
 // composeu(double, square)(3) //36
 
 function composeu( func1, func2) {
@@ -113,11 +113,30 @@ function composeu( func1, func2) {
 	};
 }
 
-//write a function composeb that takes two binary funcitons and returns afunciotn that call them both.
+//write a function composeb that takes two binary functions and returns a function that call them both.
 //so that 
 //composeb(add, mul)(2,3,5) //25
 function composeb( func1, func2 ) {
 	return function( x, y ,z ) {
 		return func2( func1(x,y), z );
+	};
+}
+
+// Write a function that allows another function to only be called once
+// So that: 
+// add_once = once(add);
+// add_once(3, 4) // 7
+// add_once(3, 4) // Throw
+
+function once( func ) {
+	return func;
+}
+
+function once( func ) {
+	var newFunc = func;
+	return function() {
+        var newFunc2 = newFunc;
+        newFunc = null;
+		return newFunc2.apply(this, arguments);
 	};
 }
